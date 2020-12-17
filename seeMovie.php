9e569197d977b119d9e9f955727432c62740650d
koma0701 <75934803+koma0701@users.noreply.github.com>
2020-12-17 09:53:12 +0900

    Add files via upload

--- seeMovie.php
+++ seeMovie.php
@@ -21,52 +21,62 @@
         $pic = $result['movie_picture'];
         $mname = $result['movie_name'];
         $msum = $result['movie_summary'];
+        $mcap = $result['movie_cap'];
+        $mprice = $result['movie_price'];
         $mid = $result['movie_id'];
-
+        
         $row2 = $mov->getMovieInfo($mid);
 
-        echo "
-        <div class='card w-75 mb-3 mx-auto'>
-            <div class='card-header bg-dark text-white'>     
-                <div class='container row'>
-                  <img src='img/$pic' class='col-6' style='width: 300px; height: 300px;'>
-                  <div class='d-flex flex-column col-6'>
-                      <h2>$mname</h2>
-                      <p class='p-3'>$msum</p>
+        if($mcap != 0){
+          echo "
+          <div class='card w-75 mb-3 mx-auto'>
+              <div class='card-header bg-dark text-white'>     
+                  <div class='container row'>
+                    <img src='img/$pic' class='col-6' style='width: 300px; height: 300px;'>
+                    <div class='d-flex flex-column col-6'>
+                        <h2>$mname</h2>
+                        <p class='p-3 border'>$msum</p>
+                        <p class='p-3 border'>Number of available seats is $mcap</p>
+                        <p class='p-3 border'>The ticket price is " .number_format($mprice,2). "</p>
+                    </div>
                   </div>
-                </div>
-            </div>
-            <div class='card-body'>";
-              foreach($row2 as $result2){
-                $mdate = $result2['ms_date'];
-                $mtime = $result2['ms_time'];
-                echo "
-                <div class='row'>
-                  <div class='col-5'>
-                    <table class='table table-striped table-bordered'>
-                        <tr>
-                          <td class=''>$mdate</td>
-                        </tr>
-                    </table>    
-                  </div>     
-                  <div class='col-5'>
-                    <table class='table table-striped table-bordered'>
+              </div>
+              <div class='card-body'>";
+                foreach($row2 as $result2){
+                  $mdate = $result2['ms_date'];
+                  $mtime = $result2['ms_time'];
+  
+                
+  
+                  echo "
+                  <div class='row'>
+                    <div class='col-5'>
+                      <table class='table table-striped table-bordered'>
                           <tr>
-                            <td class=''>$mtime</td>
+                            <td class=''>$mdate</td>
                           </tr>
-                    </table>
-                  </div>
-                  <div class='col-2'>
-                    <a href='reservation.php?picture=$pic&mname=$mname&mdate=$mdate&mtime=$mtime' class='btn btn-dark'>Reserve</a>
-                  </div>
-                </div> 
-                ";
-              }
-        echo "    
-            </div>
-        </div>
-        ";
-      }
+                      </table>    
+                    </div>     
+                    <div class='col-5'>
+                      <table class='table table-striped table-bordered'>
+                            <tr>
+                              <td class=''>$mtime</td>
+                            </tr>
+                      </table>
+                    </div>
+                    <div class='col-2'>
+                      <a href='reservation.php?picture=$pic&mname=$mname&mdate=$mdate&mtime=$mtime&mprice=$mprice&mcap=$mcap&mid=$mid' class='btn btn-dark'>Reserve</a>
+                    </div>
+                  </div> 
+                  ";
+                }
+          echo "    
+              </div>
+          </div>
+          ";
+        }
+        }
+        
   ?>
     </div>
 </body>
